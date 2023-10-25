e0fc3b3934f8df450b7a5986e3a4ad6e16d9c3ec
Sandhiyamuneeswaran <148308598+Sandhiyamuneeswaran@users.noreply.github.com>
2023-10-25 14:22:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const contentElement = document.querySelector('.content');
+
+    const colors = ['#FF5733', '#07AE98', '#3D85C6', '#E55393', '#F333FF'];
+    
+    function changeBackgroundColor() {
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+        contentElement.style.backgroundColor = randomColor;
+    }
+
+    // Change the background color every 5 seconds (5000 milliseconds)
+    setInterval(changeBackgroundColor, 5000);
+});
